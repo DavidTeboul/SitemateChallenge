fffa0d9c35c204caf8a0cfe159e8cf11c206735e
David Teboul <68842616+DavidTeboul@users.noreply.github.com>
2024-08-26 11:46:26 +0300

    Server Side dev - Init server app express Defined 4 function for the issues (creat , update ,get , delete Run the server on PORT 3000

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+
+
+//exemple of issues to pass on REST API
+let issues = [
+    { id:1,title:'Issue1',description:'Description1'},
+    { id:2,title:'Issue2',description:'Description2'},
+    { id:3,title:'Issue3',description:'Description3'},
+]
+
+//Create: accepts a JSON object & prints/logs the object
+app.post('/issues',(req,res)=>{
+    const newIssueToSend = req.body;
+    issues.push(newIssueToSend);
+    console.log('Issue Created',newIssueToSend);
+    res.status(201).json(newIssueToSend);
+
+});
+
+// Read: requests a JSON object & prints it out
+
+app.get('/issues',(req,res)=>{
+   res.json(issues);
+});
+
+//Update: sends a JSON object to the server
+
+app.put('/issues/:id',(req, res)=>{
+    const {id} = req.params;
+    const updateIssues = req.body;
+    issues = issues.map(issue => (issue.id === parseInt(id) ? updatedIssue : issue));
+    console.log('Updated Issue:', updateIssues);
+    res.json(updateIssues);
+})
+
+// Delete: requests the server delete an issue
+
+app.delete('/issues/:id',(req, res)=>{
+    const {id} = req.params;
+    const deleteIssues = req.body;
+    issues = issues.map(issue => (issue.id === parseInt(id) ? deleteIssues : issue));
+    console.log('Delete Issue:', deleteIssues);
+    res.json(deleteIssues);
+})
+
+app.listen(PORT, ()=>{
+   console.log('Server is running on PORT: $(PORT)');
+});
